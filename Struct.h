82247430bac3eefe21e43eb16d8069c72c04ccdd
kotoha <k022g1001@g.neec.ac.jp>
2022-12-27 15:57:18 +0900

    構造体

---
+++ Struct.h
@@ -0,0 +1,19 @@
+#pragma once
+
+//構造体
+typedef struct Vector2
+{
+	float x;
+	float y;
+};
+
+//マウス
+typedef struct Mouse
+{
+	int posX;					//マウスのX座標
+	int posY;					//マウスのY座標
+	int clickCount;				//カウント
+	bool isLeftClick;			//左クリック判定
+	bool isRightClick;			//右クリック判定
+};
+
