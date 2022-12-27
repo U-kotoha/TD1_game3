07c8c66a533f4cbce31201ba037ae11f25b154be
kotoha <k022g1001@g.neec.ac.jp>
2022-12-27 15:58:38 +0900

    構造体

--- Struct.h
+++ Struct.h
@@ -10,10 +10,10 @@ typedef struct Vector2
 //マウス
 typedef struct Mouse
 {
-	int posX;					//マウスのX座標
-	int posY;					//マウスのY座標
-	int clickCount;				//カウント
-	bool isLeftClick;			//左クリック判定
-	bool isRightClick;			//右クリック判定
+	int posX; //マウスのX座標
+	int posY; //マウスのY座標
+	int clickCount; //カウント
+	bool isLeftClick; //左クリック判定
+	bool isRightClick; //右クリック判定
 };
 
